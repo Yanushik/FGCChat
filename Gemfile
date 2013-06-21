c22124c5ce34ffde236d224d55cdc8d5f6476489
Yanushik Cerda <yanushikcerda@gmail.com>
2013-06-21 00:24:22 -0500

    chat server problems remedied

--- Gemfile
+++ Gemfile
@@ -4,9 +4,11 @@ gem 'rails', '3.2.13'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-
+gem 'cathodic'
+gem 'oauth'
 group :development, :test do
   gem 'sqlite3'
+  
 end
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,16 @@
 class ChatController < ApplicationController
   def chatroom
+  @servers = ["irc.arcti.ca",
+			 "irc.choopa.net",
+			 "irc.colosolutions.net",
+			 "irc.eversible.com",
+		     "irc.mzima.net",
+			 "irc.paraphysics.net",
+			 "irc.Prison.NET",
+             "irc.servercentral.net",
+			 "irc.SHOUTcast.com",
+			 "irc.teksavvy.ca",
+			 "irc.umich.edu"]
   @streamlist = Stream.all
   @default = "FGC_Cat"
   end
