0272019163b5f00d11ba0815fc1e8b7e983c40d9
Yanushik Cerda <yanushikcerda@gmail.com>
2013-06-25 11:25:17 -0500

    Various style fixes and irc server changes

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,16 +1,7 @@
 class ChatController < ApplicationController
   def chatroom
-  @servers = ["irc.arcti.ca",
-			 "irc.choopa.net",
-			 "irc.colosolutions.net",
-			 "irc.eversible.com",
-		     "irc.mzima.net",
-			 "irc.paraphysics.net",
-			 "irc.Prison.NET",
-             "irc.servercentral.net",
-			 "irc.SHOUTcast.com",
-			 "irc.teksavvy.ca",
-			 "irc.umich.edu"]
+  @servers = ["irc.arcti.ca"]
+
   @streamlist = Stream.all
   @default = "FGC_Cat"
   end
