7f011d3134c21946b2a0f65074f32e292f630fd7
Yanushik Cerda <yanushikcerda@gmail.com>
2013-06-25 20:11:29 -0500

    Added servers to irc list.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,8 +1,17 @@
 class ChatController < ApplicationController
   def chatroom
   @servers = [
-    "irc.inet.tele.dk"]
-
+    "irc.arcti.ca",
+    "irc.choopa.net",
+    "irc.colosolutions.net",
+    "irc.eversible.com",
+   "irc.mzima.net",
+    "irc.paraphysics.net",
+    "irc.Prison.NET",
+    "irc.servercentral.net",
+    "irc.SHOUTcast.com",
+    "irc.teksavvy.ca"
+  ]
   @streamlist = Stream.all
   @default = "FGC_Cat"
   end
