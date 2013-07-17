c53e01aef0986b1df67b153f7ce8b5c8bd1321f8
Yanushik Cerda <yanushikcerda@gmail.com>
2013-07-16 21:07:42 -0500

    Route changes. CSS Tweaks.

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ FGCChat::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   #root :to => 'chat#chatroom'
-	root :to => 'chat#evo'
+	root :to => 'chat#chatroom'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
